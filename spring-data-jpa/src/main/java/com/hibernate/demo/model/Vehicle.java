e3d360b7c9b4abee75a7fb236c5d6283a1d54c0a
Allah-The-Dev <pandeyneelkumar@gmail.com>
2020-08-08 22:50:41 +0530

    added many to one from vehicle to user

---
+++ spring-data-jpa/src/main/java/com/hibernate/demo/model/Vehicle.java
@@ -0,0 +1,39 @@
+package com.hibernate.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "vehicle")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Vehicle {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "registration_no")
+    private String registrationNo;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private User user;
+}
--- spring-data-jpa/src/main/resources/data.sql
+++ spring-data-jpa/src/main/resources/data.sql
@@ -3,3 +3,10 @@ insert into users (id, initials, user_name, home_addr_street, home_addr_city, ho
         (1, 'Mr.', 'Neelesh Kumar Pandey', 'street1', 'city1', 'state1', 'pin1'),
         (2, 'Mr.', 'Lalji Pandey', 'street2', 'city2', 'state2', 'pin2'),
         (3, 'Mr.', 'Abhishek Pandey', 'street3', 'city3', 'state3', 'pin3');
+
+insert into vehicle (id, registration_no, user_id) 
+    values 
+        (1, 'KA015CP241', 1),
+        (2, 'KA015CP242', 1),
+        (3, 'KA015CP243', 2),
+        (4, 'KA015CP244', 3);
