00da25b3b2b02af04e7607518f3846b6d7606cca
Allah-The-Dev <pandeyneelkumar@gmail.com>
2020-08-09 13:47:19 +0530

    using unique and nullable properties of column attribute

--- spring-data-jpa/src/main/java/com/hibernate/demo/model/User.java
+++ spring-data-jpa/src/main/java/com/hibernate/demo/model/User.java
@@ -27,6 +27,7 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", unique = true, nullable = false)
     private Long id;
     private String initials;
     
